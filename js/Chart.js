f0a679d4c3c3f7237655085294819a07f8e5c3bb
Daniel Engebretsen <danielengebretsen1@gmail.com>
2015-01-07 11:08:41 +0100

    added web interface files

---
+++ js/Chart.js
@@ -0,0 +1,29 @@
+
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'C˚');
+    data.addRows([
+        ['I dag', 26],
+    ]);
+
+    // Set chart options
+    var options = {'title':'Temperatur i Mosseveien 145',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
